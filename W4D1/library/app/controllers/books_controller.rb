709f84ae00ae444fffbe8ef08a6d8f8eab94e56c
Andrea del Rio <adelrio@gmail.com>
2017-02-12 21:54:35 -0800

    #create, #new, #destroy

--- W4D1/library/app/controllers/books_controller.rb
+++ W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    render :index
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params)
+
+    if Book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = books.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
+    book = Book.find_by(id: params[:id])
+    book.destroy
+    redirect_to books_url
   end
 
   private
