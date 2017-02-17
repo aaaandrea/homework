3e9748a6de069c1fc645cef45d3887a95757bc7a
Andrea del Rio <adelrio@gmail.com>
2017-02-17 09:35:35 -0800

    add toyable module

--- W4D5/ToyApp/app/models/concerns/toyable.rb
+++ W4D5/ToyApp/app/models/concerns/toyable.rb
@@ -2,8 +2,11 @@ module Toyable
   extend ActiveSupport::Concern
 
   included do
+    has_many :toys, as: :toyable
+    has_many :toys, through: :toyable
   end
 
   def receive_toy(name)
+    self.joins(:toys).where('toys.toyable_id' => :toy)
   end
 end
