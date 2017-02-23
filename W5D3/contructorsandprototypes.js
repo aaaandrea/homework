7a15020e0ae816b1eb11aeadc8a87ab39d2742fb
Andrea del Rio <adelrio@gmail.com>
2017-02-23 09:33:17 -0800

    callbacks updates

--- W5D3/contructorsandprototypes.js
+++ W5D3/contructorsandprototypes.js
@@ -1,3 +1,23 @@
+// First write a constructor function for an elephant. Each elephant
+// should have a name, height (in inches), and array of tricks in
+// gerund form (e.g. "painting a picture" rather than "paint a picture").
+//
+// Next write a few prototype functions that will be shared among all
+// elephants:
+//
+// Elephant.prototype.trumpet: should print "(name) the elephant goes
+// 'phrRRRRRRRRRRR!!!!!!!'"
+// Elephant.prototype.grow: should increase the elephant's height by
+// 12 inches
+// Elephant.prototype.addTrick(trick): add a new trick to their
+// existing repertoire
+// Elephant.prototype.play: print out a random trick,
+// e.g. "(name) is (trick)!"
+// Hint: look up some JS Math methods!
+// Make sure to create an elephant and test all of these functions
+// out on them method style!
+
+
 console.log("Hello from the JavaScript console!");
 
 $.ajax({
